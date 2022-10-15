8cc1ad9a78caa84257442a00d78541208fcd7e5f
252308 <ladyfsquare@yahoo.com>
2022-10-15 06:21:53 -0700

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /*
  * main - print a line of code using puts
  *
- * Return: 0
- */
-int main(void)
+ *  Return: 0
+*/
+ int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
