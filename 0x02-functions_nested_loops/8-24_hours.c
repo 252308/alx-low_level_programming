ccd3a4463a7d5da4e9da2c61f13727e2bc18cfeb
252308 <ladyfsquare@yahoo.com>
2022-10-20 13:33:01 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - prints every minute of the day of
- *	jack bauer; starting from 00:00 to 23:59.
+ *	jack bauer: starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
@@ -15,7 +15,7 @@ void jack_bauer(void)
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((minute / 10) + '0')
+			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
